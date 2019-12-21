09366249e73366d5290dad923ac405e6b34fa76b
Zaft81 <Ryan81@126.com>
2019-12-21 13:24:36 +0800

    submit

---
+++ src/main/java/com/jzweb/httputils/JzwebHttputilsApplication.java
@@ -0,0 +1,13 @@
+package com.jzweb.httputils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzwebHttputilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JzwebHttputilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jzweb/httputils/JzwebHttputilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzweb.httputils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JzwebHttputilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
