df81bf428a7c1524f138f446f0bbd919dc915c39
Zaft81 <Ryan81@126.com>
2019-12-22 11:24:17 +0800

    submit

--- src/main/java/com/jzweb/httputils/config/httpUtilsConfig.java
+++ src/main/java/com/jzweb/httputils/config/httpUtilsConfig.java
@@ -1,6 +1,12 @@
 package com.jzweb.httputils.config;
 
+import com.jzweb.httputils.vo.ApiConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Ryan
@@ -8,6 +14,17 @@ import org.springframework.context.annotation.Configuration;
  * @date 12/21/2019
  */
 @Configuration
+@PropertySource(value = "classpath:apiconfig-list.properties")
+@ConfigurationProperties(prefix = "http")
 public class httpUtilsConfig {
 
+    private List<ApiConfig> configs = new ArrayList<>();
+
+    public List<ApiConfig> getConfigs() {
+        return configs;
+    }
+
+    public void setConfigs(List<ApiConfig> configs) {
+        this.configs = configs;
+    }
 }
